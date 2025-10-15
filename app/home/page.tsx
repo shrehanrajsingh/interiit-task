acb2902774344d630ae19a6ce6d1aabc9751341d
shrehanrajsingh <shrehanofficial@gmail.com>
2025-10-15 19:30:24 +0530

    add: left bar to home page

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,7 +1,24 @@
+import LeftBar from "./leftbar";
+
 export default function Home() {
   return (
-    <div>
-      <h1>hi</h1>
+    <div className="w-full min-h-screen bg-gray-800 h-screen flex p-4">
+      <div className="h-full w-full bg-black/30 rounded-lg grow text-white grid grid-cols-14">
+        {/* left bar */}
+        <div className="col-span-3">
+          <LeftBar />
+        </div>
+
+        {/* main section */}
+        <div className="col-span-8">
+          <h1>hi</h1>
+        </div>
+
+        {/* right bar */}
+        <div className="col-span-3">
+          <h1>hi</h1>
+        </div>
+      </div>
     </div>
   );
 }
